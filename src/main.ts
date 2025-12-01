05d28131ff8ef9feccc9c44cd38357c38022397d
cw7430 <cw282@naver.com>
2025-12-01 09:42:02 +0900

    express -> fastify 교체

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,28 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import fastifyCors from '@fastify/cors';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { type ConfigType } from '@nestjs/config';
 
+import { AppModule } from './app.module';
 import { appConfig, webConfig } from './common/config';
 import { GlobalExceptionFilter } from './common/api';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  const fastifyInstance = app.getHttpAdapter().getInstance();
 
   const appConfigValue = app.get<ConfigType<typeof appConfig>>(appConfig.KEY);
   const webConfigValue = app.get<ConfigType<typeof webConfig>>(webConfig.KEY);
 
-  app.enableCors({
+  await fastifyInstance.register(fastifyCors, {
     origin: webConfigValue.CORS.ORIGIN,
     credentials: webConfigValue.CORS.CREDENTIALS,
   });
